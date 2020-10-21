519375a7352c155f1c4bdaf5f1d05ec3cec026d4
Carl Bordum Hansen <carl@bordum.dk>
2020-10-21 17:07:59 +0200

    Simplify authorization_required

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from functools import wraps
 
-from flask import Flask, jsonify, request
+from flask import Flask, current_app, jsonify, request
 
 
 app = Flask(__name__)
@@ -10,34 +10,31 @@ app = Flask(__name__)
 app.config["hs_token"] = "b3b05236568ab46f0d98a978936c514eac93d8f90e6d5cd3895b3db5bb8d788b"
 
 
-def authorization_required(app):
+def authorization_required(f):
     """Make sure that the homeserver passed the correct hs_token.
 
     Respond with M_FORBIDDEN, if the token does not match.
 
     Usage:
-        @authorization_required(app)
+        @authorization_required
         def my_func_that_requires_authorization():
             ...
     """
 
-    def inner(f):
-        @wraps(f)
-        def innerinner(*args, **kwargs):
-            token = request.args.get("access_token", False)
-            hs_token = app.config["hs_token"]
-            if not token or token != hs_token:
-                return jsonify({"errcode": "CHAT.CACTUS.APPSERVICE_FORBIDDEN"}), 403
-            return f(*args, **kwargs)
-
-        return innerinner
+    @wraps(f)
+    def inner(*args, **kwargs):
+        token = request.args.get("access_token", False)
+        hs_token = current_app.config["hs_token"]
+        if not token or token != hs_token:
+            return jsonify({"errcode": "CHAT.CACTUS.APPSERVICE_FORBIDDEN"}), 403
+        return f(*args, **kwargs)
 
     return inner
 
 
 @app.route("/transactions/<string:txn_id>", methods=["PUT"])  # deprecated
 @app.route("/_matrix/app/v1/transactions/<string:txn_id>", methods=["PUT"])
-@authorization_required(app)
+@authorization_required
 def new_transaction(txn_id: str):
     """Implement the Push API from the appservice specification.
 
@@ -51,7 +48,7 @@ def new_transaction(txn_id: str):
 
 @app.route("/rooms/<path:alias>", methods=["GET"])  # deprecated
 @app.route("/_matrix/app/v1/rooms/<path:alias>", methods=["GET"])
-@authorization_required(app)
+@authorization_required
 def query_room_alias(alias: str):
     """Implement the Room Alias Query API from the appservice specification.
 
