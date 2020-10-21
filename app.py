6beeec9ae683a50e45373cfb955fc6f658c39141
Carl Bordum Hansen <carl@bordum.dk>
2020-10-21 16:35:50 +0200

    Make sure homeserver is authorized

--- app.py
+++ app.py
@@ -1,11 +1,43 @@
+from functools import wraps
+
 from flask import Flask, jsonify, request
 
 
 app = Flask(__name__)
 
 
+# TODO: configuration handling
+app.config["hs_token"] = "b3b05236568ab46f0d98a978936c514eac93d8f90e6d5cd3895b3db5bb8d788b"
+
+
+def authorization_required(app):
+    """Make sure that the homeserver passed the correct hs_token.
+
+    Respond with M_FORBIDDEN, if the token does not match.
+
+    Usage:
+        @authorization_required(app)
+        def my_func_that_requires_authorization():
+            ...
+    """
+
+    def inner(f):
+        @wraps(f)
+        def innerinner(*args, **kwargs):
+            token = request.args.get("access_token", False)
+            hs_token = app.config["hs_token"]
+            if not token or token != hs_token:
+                return jsonify({"errcode": "CHAT.CACTUS.APPSERVICE_FORBIDDEN"}), 403
+            return f(*args, **kwargs)
+
+        return innerinner
+
+    return inner
+
+
 @app.route("/transactions/<string:txn_id>", methods=["PUT"])  # deprecated
 @app.route("/_matrix/app/v1/transactions/<string:txn_id>", methods=["PUT"])
+@authorization_required(app)
 def new_transaction(txn_id: str):
     events = request.get_json()["events"]
     for event in events:
@@ -15,6 +47,7 @@ def new_transaction(txn_id: str):
 
 @app.route("/rooms/<path:alias>", methods=["GET"])  # deprecated
 @app.route("/_matrix/app/v1/rooms/<path:alias>", methods=["GET"])
+@authorization_required(app)
 def query_room_alias(alias: str):
     """Implement the Room Alias Query API from the appservice specification.
 
--- test_app.py
+++ test_app.py
@@ -6,25 +6,60 @@ from app import app
 @pytest.fixture
 def appservice():
     with app.test_client() as c:
+
+        def authorized_request(*args, **kwargs):
+            # Inject access token in query string
+            query = kwargs.get("query_string", {})
+            query["access_token"] = app.config["hs_token"]
+            kwargs["query_string"] = query
+
+            return c.open(*args, **kwargs)
+
+        c.authorized_request = authorized_request
         yield c
 
 
 def test_query_room_alias_200(appservice):
-    r = appservice.get("/_matrix/app/v1/rooms/_comments_hi_there")
+    r = appservice.authorized_request("/_matrix/app/v1/rooms/_comments_hi_there")
     assert r.status_code == 200
     assert r.get_json() == {}
 
 
 def test_query_room_alias_slashed(appservice):
-    r = appservice.get("/_matrix/app/v1/rooms/_comments_hi/there")
+    r = appservice.authorized_request("/_matrix/app/v1/rooms/_comments_hi/there")
     assert r.status_code == 200
     assert r.get_json() == {}
 
 
 def test_push_api_empty_success(appservice):
-    r = appservice.put(
-        "/_matrix/app/v1/transactions/42",
-        json={"events": []},
+    r = appservice.authorized_request(
+        "/_matrix/app/v1/transactions/42", method="PUT", json={"events": []},
     )
     assert r.status_code == 200
     assert r.get_json() == {}
+
+
+def test_unauthorized_query_room_alias(appservice):
+    r = appservice.get("/_matrix/app/v1/rooms/_comments_hi_there")
+    assert r.status_code == 403
+    assert r.get_json() == {"errcode": "CHAT.CACTUS.APPSERVICE_FORBIDDEN"}
+
+
+def test_unauthorized_push_api_call(appservice):
+    r = appservice.put(
+        "/_matrix/app/v1/transactions/42",
+        query_string={"access_token": "not_the_real_access_token"},
+        json={"events": []},
+    )
+    assert r.status_code == 403
+    assert r.get_json() == {"errcode": "CHAT.CACTUS.APPSERVICE_FORBIDDEN"}
+
+
+def test_unauthorized_push_api_call_empty_token(appservice):
+    r = appservice.put(
+        "/_matrix/app/v1/transactions/42",
+        query_string={"access_token": ""},
+        json={"events": []},
+    )
+    assert r.status_code == 403
+    assert r.get_json() == {"errcode": "CHAT.CACTUS.APPSERVICE_FORBIDDEN"}
