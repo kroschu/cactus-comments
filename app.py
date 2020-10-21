837ca52cb4ff0e05de02b6789728af7938f23eea
Carl Bordum Hansen <carl@bordum.dk>
2020-10-21 11:43:55 +0200

    Register appservice
    app.py just prints all transactions for now

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 
 app = Flask(__name__)
+
+
+@app.route("/transactions/<string:txn_id>", methods=["PUT"])  # deprecated
+@app.route("/_matrix/app/v1/transactions/<string:txn_id>", methods=["PUT"])
+def new_transaction(txn_id: str):
+    events = request.get_json()["events"]
+    for event in events:
+        print(txn_id, event)
+    return jsonify({})
