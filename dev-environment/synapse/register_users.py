364841f835c49cda56fda8d434eaabf0ca2ff5d8
Carl Bordum Hansen <carl@bordum.dk>
2021-07-14 15:24:47 +0200

    Lower complexity of register_users slightly

--- dev-environment/synapse/register_users.py
+++ dev-environment/synapse/register_users.py
@@ -4,10 +4,13 @@ import time
 
 def register(username):
     """Ensure that `username` is registered.
+
     Terminates on successful register, or if user is already registered.
     Retries on connection error. Useful while synapse is starting.
     """
+
     print(f"🔧 Registering user: {username}")
+
     while True:
         try:
             r = requests.post(
@@ -18,16 +21,20 @@ def register(username):
                     "password": username,
                 },
             )
-            if r.status_code == 200:
-                print(f"🔧 Successfully registered user: {username}")
-                return
-            if r.status_code == 400 and r.json()["errcode"] == "M_USER_IN_USE":
-                print(f"🔧 User already registered: {username}")
-                return
-            else:
-                print(f"🔧 Bad status when registering: {r.status_code}, {r.text}")
         except Exception as e:
             print(f"🔧 Error registering: {e}")
+            time.sleep(2)
+            continue
+
+        if r.status_code == 200:
+            print(f"🔧 Successfully registered user: {username}")
+            return
+
+        if r.status_code == 400 and r.json()["errcode"] == "M_USER_IN_USE":
+            print(f"🔧 User already registered: {username}")
+            return
+
+        print(f"🔧 Bad status when registering: {r.status_code}, {r.text}")
         time.sleep(2)
 
 
