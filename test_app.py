cef9a42c9516656b4da1cbd72f0d36e5f397ad5d
AsbjornOlling <asbjornolling@gmail.com>
2022-02-04 09:26:12 +0100

    add test for automatic invites, bump synapse in dev env

--- test_app.py
+++ test_app.py
@@ -8,11 +8,12 @@ from app import create_app_from_env
 
 
 @pytest.fixture
-def sitename():
-    """Register `sitename` as user "dev1"."""
-    sitename = str(uuid.uuid4())
-    homeserver_url = os.environ["CACTUS_HOMESERVER_URL"]
-    userid, password = "@dev1:localhost:8008", "dev1"
+def homeserver_url():
+    return os.environ["CACTUS_HOMESERVER_URL"]
+
+
+def sign_in(homeserver_url, username):
+    userid, password = f"@{username}:localhost:8008", username
 
     # log in
     r = requests.post(
@@ -26,6 +27,27 @@ def sitename():
     assert r.status_code == 200, f"Login as {userid} failed: {r.status_code}"
     access_token = r.json()["access_token"]
     server_name = r.json()["home_server"]
+    headers = {"Authorization": f"Bearer {access_token}"}
+    return access_token
+
+
+@pytest.fixture
+def dev1(homeserver_url):
+    return sign_in(homeserver_url, "dev1")
+
+
+@pytest.fixture
+def dev2(homeserver_url):
+    return sign_in(homeserver_url, "dev2")
+
+
+@pytest.fixture
+def sitename(homeserver_url, dev1):
+    """Register `sitename` as user "dev1"."""
+    sitename = str(uuid.uuid4())
+    server_name = "localhost:8008"
+    userid, password = "@dev1:localhost:8008", "dev1"
+    access_token = dev1
     headers = {"Authorization": f"Bearer {access_token}"}
 
     # create a new room
@@ -97,7 +119,6 @@ def appservice():
             query = kwargs.get("query_string", {})
             query["access_token"] = app.config["hs_token"]
             kwargs["query_string"] = query
-
             return c.open(*args, **kwargs)
 
         c.authorized_request = authorized_request
@@ -192,3 +213,33 @@ def test_unauthorized_push_api_call_empty_token(appservice):
     )
     assert r.status_code == 401
     assert r.get_json() == {"errcode": "CHAT.CACTUS.APPSERVICE_UNAUTHORIZED"}
+
+
+def test_auto_invite(homeserver_url, appservice, sitename, dev1, dev2):
+    """ When the appservice creates a new room,
+        the mod room participants are invited to it """
+
+    # get the appservice to make a new room
+    random_id = str(uuid.uuid4())[:8]
+    encoded_alias = f"%23comments_{sitename}_{random_id}%3Alocalhost%3A8008"
+    r = appservice.authorized_request(
+        f"/_matrix/app/v1/rooms/{encoded_alias}"
+    )
+    assert r.status_code == 200
+    assert r.get_json() == {}
+
+    # first, join as dev2...
+    r = requests.post(
+        f"{homeserver_url}/_matrix/client/v3/join/{encoded_alias}",
+        headers={"Authorization": f"Bearer {dev2}"}
+    )
+    assert r.status_code == 200
+    room_id = r.json()["room_id"]
+
+    # ...then get the membership state event for dev1
+    r = requests.get(
+        f"{homeserver_url}/_matrix/client/v3/rooms/{room_id}/state/m.room.member/%40dev1%3Alocalhost%3A8008",
+        headers={"Authorization": f"Bearer {dev2}"}
+    )
+    assert r.status_code == 200, r.json()
+    assert r.json()["membership"] == "invite", "dev1 was not invited"
